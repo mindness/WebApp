24e153a1fcc6e6df426a279d6998d122afcb1d24
Mindness <docteura@hotmail.fr>
2019-11-12 10:09:18 +0100

    init

---
+++ src/main/java/com/mindness/websitebdd/WebsitebddApplication.java
@@ -0,0 +1,13 @@
+package com.mindness.websitebdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsitebddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsitebddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mindness/websitebdd/websitebdd/WebsitebddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindness.websitebdd.websitebdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsitebddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
