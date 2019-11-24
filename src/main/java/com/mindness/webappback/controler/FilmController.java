4dbe74aec2a70786ff059de359d94b8aff8766c2
raphael lefrancois <lefrancois.raphael@hotmail.fr>
2019-11-24 16:53:15 +0100

    stockage de la date au bon format

--- src/main/java/com/mindness/webappback/controler/FilmController.java
+++ src/main/java/com/mindness/webappback/controler/FilmController.java
@@ -1,7 +1,13 @@
 package com.mindness.webappback.controler;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 import javax.validation.Valid;
@@ -17,6 +23,8 @@ import org.springframework.web.bind.annotation.*;
 @RestController @CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/api/v1")
 public class FilmController {
+    LocalDate ldt;
+
     @Autowired
     private FilmRepository filmRepository;
 
@@ -41,13 +49,14 @@ public class FilmController {
 
     @PutMapping("/films/{id}")
     public ResponseEntity<Film> updateFilm(@PathVariable(value = "id") Long filmId,
-                                                   @Valid @RequestBody Film filmDetails) throws ResourceNotFoundException {
+                                           @Valid @RequestBody Film filmDetails) throws ResourceNotFoundException, ParseException {
         Film film = filmRepository.findById(filmId)
                 .orElseThrow(() -> new ResourceNotFoundException("Film not found for this id :: " + filmId));
 
+        ldt = LocalDate.parse(filmDetails.getDateSortie().substring(0,10), DateTimeFormatter.ofPattern("yyyy-MM-dd")).plusDays(1); //  add 1 jours pour compenser le fait que java en enleve 1
         film.setName(filmDetails.getName());
         film.setAffiche(filmDetails.getAffiche());
-        film.setDateSortie(filmDetails.getDateSortie());
+        film.setDateSortie(DateTimeFormatter.ofPattern("dd/MM/yyyy", Locale.FRANCE).format(ldt));
         film.setNote(filmDetails.getNote());
         film.setSynopsis(filmDetails.getSynopsis());
         final Film updatedFilm = filmRepository.save(film);
